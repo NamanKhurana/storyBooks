7abbb7b4aede4ff409f08eb7fb3edbdd5015f50f
NamanKhurana <khurananaman06@gmail.com>
2019-04-20 01:13:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Yup It Works")
+})
+
+const port = process.env.port || 5001;
+
+app.listen(port,()=>{
+    console.log(`Server started on port http://localhost:${port}`)
+})
+
